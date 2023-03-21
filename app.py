f3e259841acee6113cb2fc3299822350225accc3
Andrea Geremicca <geremicca.andrea@iisgalvanimi.edu.it>
2023-03-21 18:56:14 +0000

    commit

--- app.py
+++ app.py
@@ -5,17 +5,23 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login' , methods = ["GET"])
 def login():
-    return render_template('login.html')
+     
+     return render_template('login.html')
 
-@app.route('/squadraPreferita')
+@app.route('/squadraPreferita', methods = ["GET"])
 def squadraPreferita():
-    return render_template('squadrapreferita.html')
+    squadre = request.args.get("squadra") #si prende la variabile scritta nell' HTML
+    print(squadre)
+    if squadre == "Napoli": #si prende la variabile scritta nell' HTML
+        return render_template("squadrapreferitaNapoli.html")
+    else:
+        return render_template('squadrapreferitaMilan.html')
 
 @app.route('/formazioneMilan')
 def formazioneMilan():
     return render_template('formazioneMilan.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+  app.run(host='0.0.0.0', port=3246, debug=True)
